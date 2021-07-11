cf30f5b8953111116c74c8bb35593a2380393300
talysonxx <talysonoficial12@gmail.com>
2021-07-11 15:36:46 -0300

    bug das imagens arrumado

--- js/main.js
+++ js/main.js
@@ -5,107 +5,107 @@ let sinopse = window.document.querySelector('p.sinopse')
 
 // funcoes que são chamadas quando o filme é clicado
 function reZero(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/re-zero/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/re-zero/capa.jpg');"
     titulo.innerText = 'Re:Zero kara Hajimeru Isekai Seikatsu'
     sinopse.textContent = 'Natsuki Subaru, um adolescente comum, conhece uma linda garota de cabelos prateados vinda de outro mundo. Subaru quer ficar ao lado dela, mas o fardo que ela carrega é maior do que Subaru pode imaginar. Eles enfrentam o feroz ataque de monstros, traições, violência irracional... e, por fim, a morte. Subaru promete derrotar qualquer inimigo, qualquer destino, tudo para protegê-la. E assim, o pobre garoto sem poder algum obtém o "Retorno da Morte", uma habilidade única que permite ao usuário voltar no tempo ao morrer. Usando esse poder, o passado é perdido e as memórias são reescritas.'
 }
 function progressive(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/progressive/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/progressive/capa.jpg');"
     titulo.innerText = 'Sword Art Online - Progressive'
     sinopse.textContent = 'Um mês após o jogo da morte de Kayaba Akihiko começar, o número de mortos continua a aumentar, dois mil jogadores já perderam suas vidas no ultra-difícil mundo VRMMO de Sword Art Online. No dia da reunião de estratégia para planejar a batalha contra o chefe do primeiro andar, Kirito, um jogador solo que jura lutar sozinho para ficar mais forte, encontra uma rara jogadora de alto nível. Ela graciosamente despacha monstros poderosos com um único florete que brilha como uma estrela cadente na noite ...'
 }
 function hunterHunter(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/hunter-hunter/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/hunter-hunter/capa.jpg');"
     titulo.innerText = 'Hunter x Hunter'
     sinopse.textContent = 'Esse é um remake da série original. Gon é um jovem que vive em uma pequena ilha. Seu sonho é tornar-se um Hunter como o pai, para procurar tesouros, artefatos e criaturas míticas. Determinado a se tornar um Hunter profissional e a colocar sua vida em risco ao desafiar o desconhecido, ele parte em uma jornada e encontra pessoas que se acham caçadores: Killua, Kurapika e Leorio.'
 }
 function horimiya(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/horimiya/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/horimiya/capa.jpg');"
     titulo.innerText = 'Horimiya'
     sinopse.textContent = 'Embora seja admirada na escola por sua amabilidade e proeza acadêmica, a estudante Kyouko Hori esconde outro lado dela. Com os pais muitas vezes longe de casa devido ao trabalho, Hori tem que cuidar do irmão mais novo e fazer as tarefas domésticas, não deixando chance de socializar fora da escola. Enquanto isso, Izumi Miyamura é vista como um otaku taciturno de óculos. No entanto, na realidade, ele é uma pessoa gentil, incapaz de estudar. Além disso, ele tem nove piercings escondidos atrás de seu cabelo comprido e uma tatuagem nas costas e no ombro esquerdo. Por mero acaso, Hori e Miyamura se cruzam fora da escola - nenhum parecendo o que o outro esperava. Esses opostos aparentemente polares tornam-se amigos, compartilhando um com o outro um lado que nunca mostraram a ninguém.'
 }
 function narutoShippuden(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/naruto-shippuden/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/naruto-shippuden/capa.jpg');"
     titulo.innerText = 'Naruto Shippuden'
     sinopse.textContent = "Naruto Shippuden ocorre 2 anos e meio após Naruto ter ficado para treinar com Jiraiya. Após seu retorno, Naruto descobre que seus amigos shinobi's o superaram na classificação, e ele caiu para trás. No entanto, com apenas 6 meses para resgatar Sasuke, Naruto tem de enfrentar inimigos ainda mais perigosos. O plano da Akatsuki se revela lentamente e os perigos surgem mais do que nunca!"
 }
 function steinsGate(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/steins-gate/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/steins-gate/capa.jpg');"
     titulo.innerText = 'Steins;Gate'
     sinopse.textContent = 'Rintaro é um auto-proclamado cientista louco que acredita que uma organização internacional está conspirando para remodelar o mundo de acordo com seus próprios interesses. Ele e seu amigo Itaru inadvertidamente criam um dispositivo capaz de enviar mensagens para o passado. A descoberta e experimentação deste instrumento torna-se o catalisador de alterações fundamentais para o presente. Alheio das consequências de suas ações, Rintaro e seus amigos acabam criando modificações de proporções graves. Agora ele deve tentar encontrar uma maneira de voltar a linha do tempo para o mais próximo possível do original, para salvar os preciosos membros do laboratório.'
 }
 function erased(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/erased/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/erased/capa.jpg');"
     titulo.innerText = 'Erased'
     sinopse.textContent = 'O mangaká novato Fujinuma Satoru é obstruído pelo seu medo de se expressar. No entanto, ele possui um talento sobrenatural de ser forçado a evitar mortes e catástrofes ao ser enviado de volta no tempo antes da ocorrência do acidente, se repetindo até que o acidente seja impedido. Um dia, ele se envolve em um acidente que ele mesmo é enquadrado como um assassino. Desesperado para salvar a vítima, ele volta no tempo só para encontrar a si mesmo como um aluno do primário, um mês antes de sua colega de classe Hinadzuki Kayo desaparecer. Satoru agora embarca em uma nova missão: salvar Kayo e resolver o mistério por trás do seu desaparecimento.'
 }
 function atackOnTitan(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/snk/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/snk/capa.jpg');"
     titulo.innerText = 'Shingeki no Kyojin'
     sinopse.textContent = 'Titãs estão quase exterminando a raça humana. Porém alguns estão dispostos a formar um exército de ataque aos seres assassinos. O jovem Eren, após ver sua mãe ser devorada por um titã, decide que não deixará nenhum deles vivo e buscará sua vingança completa.'
 }
 function yourName(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/your-name/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/your-name/capa.jpg');"
     titulo.innerText = 'Your Name'
     sinopse.textContent = 'Mitsuha é a filha do prefeito de uma pequena cidade, mas sonha em tentar a sorte em Tóquio. Taki trabalha em um restaurante em Tóquio e deseja largar o seu emprego. Os dois não se conhecem, mas estão conectados pelas imagens de seus sonhos.'
 }
 function kOn(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/k-on/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/k-on/capa.jpg');"
     titulo.innerText = 'K-ON!'
     sinopse.textContent = 'Sua história é focada nas ações de um clube escolar de música formado por quatro garotas do ensino médio japonês. Tudo começa quando a protagonista Hirasawa Yui entra no clube sem saber tocar nenhum instrumento.'
 }
 function kanojoOkarishimasu(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/kanojo-okarishimasu/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/kanojo-okarishimasu/capa.jpg');"
     titulo.innerText = 'Kanojo, okarashimasu'
     sinopse.textContent = 'Abandonado por sua namorada, Kazuya Kinoshita, um estudante emocionalmente abalado, tenta apaziguar o vazio em seu coração através de uma namorada alugada de um aplicativo móvel. ... Incapaz de dizer a verdade, Kazuya e Chizuru são forçados a um relacionamento falso – agindo como se fossem verdadeiros namorados.'
 }
 function kizuMonogatari(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/kizu-monogatari/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/kizu-monogatari/capa.jpg');"
     titulo.innerText = 'Kizu Monogatari'
     sinopse.textContent = 'Este romance apresenta a vida de Koyomi Araragi antes dos eventos em Hitagi Caranguejo. Ele detalha o encontro de Araragi com o vampiro Kiss-shot Acerola-orion Heart-under-blade e sua viagem de volta para a humanidade de ser um vampiro.'
 }
 function kuzuNoHonkai(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/kuzu-no-honkai/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/kuzu-no-honkai/capa.jpg');"
     titulo.innerText = 'Kuzu no Honkai'
     sinopse.textContent = 'A história segue Hanabi e Mugi que causam muitos olhares invejosos como um belo casal com excelente conduta moral. Mas estas duas pessoas aparentemente perfeitas na sua aparentemente perfeita relação compartilham um segredo que não podem contar a ninguém.'
 }
 function maidDragon(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/kobayashi-san-chi/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/kobayashi-san-chi/capa.jpg');"
     titulo.innerText = 'Kobayashi-san Chi no Maid Dragon'
     sinopse.textContent = 'Kobayashi vivia sozinha em um apartamento, até que um dia, Tooru apareceu e elas acabaram vivendo juntas. Tooru, que é uma garota dragão, encara os seres humanos como inferiores e tolos, mas como foi salva por Kobayashi-san, ela faz tudo o que pode para pagar sua dívida e a ajuda com várias coisas, embora nem tudo saia de acordo com o planejado.'
 }
 function angelBeats(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/angel-beats/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/angel-beats/capa.jpg');"
     titulo.innerText = 'Angel Beats!'
     sinopse.textContent = 'Otonashi acorda e descobre que está morto. Uma menina chamada Yuri explica que eles estão na vida após a morte, e Otonashi percebe que a única coisa que ele se lembra de si mesmo é o seu nome. Yuri lhe conta que ela lidera a Shinda Sekai Sensen e guerreia contra uma garota chamada Tenshi. Incapaz de acreditar que Tenshi é maligna, Otonashi tenta falar com ela, mas o encontro não ocorre como imaginava. Otonashi decide juntar-se a SSS e lutar contra Tenshi, mas esta estranhamente atraído por ela. Ao tentar recuperar suas memórias e compreender Tenshi, ele gradualmente desvenda os mistérios da vida após a morte.'
 }
 function dororo(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/dororo/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/dororo/capa.jpg');"
     titulo.innerText = 'Dororo'
     sinopse.textContent = 'Um senhor samurai trocou os órgãos do seu filho recém-nascido por 48 demônios em troca do domínio no campo de batalha. No entanto, o bebê abandonado sobrevive graças a um curandeiro que o equipa com próteses primitivas – armas letais que o garoto usará para caçar a multidão de demônios para recuperar seu corpo um pedaço de cada vez, antes de confrontar seu pai. Em suas viagens, o jovem herói encontra um órfão que afirma ser o maior ladrão do Japão.'
 }
 function drStone(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/dr-stone/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/dr-stone/capa.jpg');"
     titulo.innerText = 'Dr. Stone'
     sinopse.textContent = 'Em um instante, todos os humanos do mundo viraram pedra. Este acontecimento misterioso envolveu o estudante Taiju, depois de milhares de anos, ele e o seu amigo Senku despertam e decidem reconstruir a civilização do zero! E assim começa uma grande e única aventura de sobrevivência num mundo de ficção científica!'
 }
 function josee(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/josee/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/josee/capa.jpg');"
     titulo.innerText = 'Josee to Tora to Sakana-tachi'
     sinopse.textContent = 'A história centra-se na relação entre Tsuneo e Josee. Tsuneo é um estudante universitário e Josee é uma jovem rapariga que, raramente saiu de casa por si mesma, devido a sua incapacidade de andar. Os dois conhecem-se quando Tsuneo encontra a avó de Josee a levá-la numa “caminhada matinal”.'
 }
 function fateStayNight(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/fate-stay-night/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/fate-stay-night/capa.jpg');"
         titulo.innerText = 'Fate/Stay Night: Unlimited Blade Works'
         sinopse.textContent = 'Este filme conta um dos finais alternativos de Fate/Stay Night, em que se segue a rota de Rin e o principal servo é Archer.'
 }
 function killLaKill(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/kill-la-kill/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/kill-la-kill/capa.jpg');"
     titulo.innerText = 'Kill la Kill'
     sinopse.textContent = 'Na Academia Honnouji há um grupo de estudantes com poderes sobrenaturais obtidos através de um uniforme conhecido como "Uniforme Goku". Com o poder do uniforme, o representante dos estudantes, Satsuki Kiryuin, dita as regras com autoridade e temor. Ryuko confronta Satsuki para obter informação do segredo a respeito das lâminas-tesoura, mas... O encontro deles foi por acaso ou estavam destinados a isso? O confronto de ambos envolverá toda a academia!'
 }
 function koeNoKatachi(){
-    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('../sources/img/koe-no-katachi/capa.jpg');"
+    posterFilme.style = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url('./sources/img/koe-no-katachi/capa.jpg');"
     titulo.innerText = 'A Voz do Silêncio'
     sinopse.textContent = 'Nishimiya Shouko é uma estudante com deficiência auditiva. Durante o ensino fundamental, após se transferir para uma nova escola, Shouko passa a ser alvo de bullying e em pouco tempo precisa se transferir. O que ela não esperava é que alguns anos depois, Ishida Shouya, um dos valentões que tanto a fez sofrer no passado surgisse de novo em sua vida com um novo propósito.'
 }
